fd28e087ea63407f47496a724bcf06ef78e17ef7
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2025-10-12 22:38:05 +0530

    typo

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -71,7 +71,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
         "from_user_id to_user_id"
       );
 
-      const subject = `You've got a new connection request from ${connection.from_user_id}`;
+      const subject = `You've got a new connection request from ${connection.from_user_id.full_name}`;
       const body = `<!DOCTYPE html>
       <html>
         <body style="font-family: Arial, sans-serif; background-color: #f5f5f5; margin: 0; padding: 30px;">
