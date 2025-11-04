313cd577441ab9c0fb6fc5966705aa6582114efc
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2025-11-04 20:50:15 +0530

    fix active tab condition

--- frontend/src/components/MenuItems.jsx
+++ frontend/src/components/MenuItems.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom";
 import { menuItemsData } from "../assets/assets";
+import { useParams } from "react-router-dom";
 
 export default function MenuItems({ setSidebarOpen }) {
+  const { profileId } = useParams();
+
   return (
     <div className="text-gray-700 space-y-2 font-semibold">
       {menuItemsData.map(({ to, label, Icon }) => (
@@ -10,11 +13,15 @@ export default function MenuItems({ setSidebarOpen }) {
           to={to}
           end={to === "/"}
           onClick={() => setSidebarOpen(false)}
-          className={({ isActive }) =>
-            `px-4 py-3 flex items-center gap-4 rounded-full ${
-              isActive ? "bg-blue-50 text-blue-800" : "hover:bg-gray-50"
-            }`
-          }
+          className={({ isActive }) => {
+            const isOwnProfile = label === "Profile" && !profileId && isActive;
+
+            return `px-4 py-3 flex items-center gap-4 rounded-full ${
+              isActive && (label !== "Profile" || isOwnProfile)
+                ? "bg-blue-50 text-blue-800"
+                : "hover:bg-gray-50"
+            }`;
+          }}
         >
           <Icon className="w-6 h-6" />
           {label}
